bb6053bc4c32097cb27c2c11c1dc615f878ac8ae
Zairo <zairooo225@gmail.com>
2026-06-07 22:27:27 +0300

    Delete src directory

--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import firebaseConfig from "../firebase-applet-config.json";
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-
-export { auth, googleProvider, signInWithPopup };
