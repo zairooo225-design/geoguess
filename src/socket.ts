1aebcf9038aa384dec3b7df045c0cf998024319d
Zairo <zairooo225@gmail.com>
2026-06-08 10:57:44 +0300

    Delete src directory

--- src/socket.ts
+++
@@ -1,9 +0,0 @@
-import { io, Socket } from "socket.io-client";
-
-// Get the app URL if it exists, otherwise default to current origin
-const SERVER_URL = import.meta.env.VITE_APP_URL || "https://ais-pre-ej2cpsom2qdbywrb6c5irf-216660484941.europe-west2.run.app";
-
-export const socket: Socket = io(SERVER_URL, {
-    autoConnect: false,
-    transports: ["websocket", "polling"]
-});
